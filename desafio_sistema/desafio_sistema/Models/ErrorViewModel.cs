6adc8bed51523b081cf1022639f292b51b184694
brayanbertan <brayanbertan@gmail.com>
2022-03-16 18:51:07 -0300

    DESAFIO - criação do projeto

---
+++ desafio_sistema/desafio_sistema/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace desafio_sistema.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
